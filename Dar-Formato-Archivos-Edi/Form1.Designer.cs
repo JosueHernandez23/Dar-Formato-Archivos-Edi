05042f260a0ed5953a812c80f310836577ec53c3
Josue Yael Moreno Hernandez <desarrollohg03@hgtransportaciones.com>
2023-01-06 10:44:47 -0600

    - Ajuste para Form1 para que la ventana se pudiera hacer reajustable

--- Dar-Formato-Archivos-Edi/Form1.Designer.cs
+++ Dar-Formato-Archivos-Edi/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.txtNombreArchivo = new System.Windows.Forms.Label();
             this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -56,6 +55,7 @@
             this.btnListadoSegmentos = new System.Windows.Forms.Button();
             this.tableLayoutPanel6 = new System.Windows.Forms.TableLayoutPanel();
             this.btnDirectorioSFTP = new System.Windows.Forms.Button();
+            this.txtNombreArchivo = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
@@ -77,19 +77,6 @@
             this.label1.Size = new System.Drawing.Size(97, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre archivo: ";
-            // 
-            // txtNombreArchivo
-            // 
-            this.txtNombreArchivo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtNombreArchivo.AutoSize = true;
-            this.txtNombreArchivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNombreArchivo.Location = new System.Drawing.Point(206, 0);
-            this.txtNombreArchivo.Name = "txtNombreArchivo";
-            this.txtNombreArchivo.Size = new System.Drawing.Size(260, 34);
-            this.txtNombreArchivo.TabIndex = 1;
-            this.txtNombreArchivo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // fileSystemWatcher1
             // 
@@ -469,6 +456,19 @@
             this.btnDirectorioSFTP.UseVisualStyleBackColor = true;
             this.btnDirectorioSFTP.Click += new System.EventHandler(this.btnDirectorioSFTP_Click);
             // 
+            // txtNombreArchivo
+            // 
+            this.txtNombreArchivo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNombreArchivo.AutoSize = true;
+            this.txtNombreArchivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreArchivo.Location = new System.Drawing.Point(206, 0);
+            this.txtNombreArchivo.Name = "txtNombreArchivo";
+            this.txtNombreArchivo.Size = new System.Drawing.Size(260, 34);
+            this.txtNombreArchivo.TabIndex = 1;
+            this.txtNombreArchivo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
             // Form1
             // 
             this.AllowDrop = true;
@@ -478,7 +478,6 @@
             this.ClientSize = new System.Drawing.Size(947, 600);
             this.Controls.Add(this.tableLayoutPanel1);
             this.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(933, 501);
             this.Name = "Form1";
@@ -505,7 +504,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label txtNombreArchivo;
         private System.IO.FileSystemWatcher fileSystemWatcher1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton rbDeshabilitar;
@@ -531,6 +529,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnReporteEventos;
         private System.Windows.Forms.Button btnDirectorioSFTP;
+        private System.Windows.Forms.Label txtNombreArchivo;
     }
 }
 
